b51b43b89d3db729c0c2f249435e9a3565947e02
Thomas Welsh <thomas.r.welsh@gmail.com>
2013-05-07 11:01:08 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Magellan::Application.config.secret_token = 'af46540d0f41a50502d74f9b3cfc303ef0789423cbd6e96a1746881e30168e1ffc0994957d6edc336a94d1daeda5c325da6f195bad458b701e73952cf14fc744'
